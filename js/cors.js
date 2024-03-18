4e9359e93b3fc898babe5c3e682eca2fc1c2165e
UNKNWNCO <xyesidx@gmail.com>
2024-03-17 22:43:26 -0500

    cors fix

--- js/cors.js
+++ js/cors.js
@@ -1,12 +1,17 @@
-function CORSSolve(){
-    const xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function(){
-      if (this.readyState == 4 && this.status == 200){
-        document.getElementById('data').innerText = xhttp.responseText
-      }
-    }
-    xhttp.open("GET","https://cors-anywhere.herokuapp.com/https://tiny-news-api.herokuapp.com/api/news",true)
-    xhttp.send()
-  }
-  
-  CORSSolve ()
+// URL del recurso al que deseas acceder
+var url = 'https://unknwnco.github.io/Price2.html';
+
+// URL del servicio de proxy CORS Anywhere
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+// Realiza la solicitud a través del proxy
+fetch(proxyUrl + url)
+  .then(response => response.json())
+  .then(data => {
+    // Procesa los datos recibidos
+    console.log(data);
+  })
+  .catch(error => {
+    // Maneja errores
+    console.error('Error fetching data:', error);
+  });
