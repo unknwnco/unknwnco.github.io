1e16d0b65507164be755fc8fb42461d381184b5b
UNKNWNCO <xyesidx@gmail.com>
2024-03-18 19:06:43 -0500

    .

--- js/script3d.js
+++ js/script3d.js
@@ -111,40 +111,35 @@
           controls1.update(); // Actualizar los controles
         });
 
-        loader.load('/3dmodels/outfit.glb', function (gltf) {
+
+        loader.load('/3dmodels/outfit_test.glb', function (gltf) {
           const model2 = gltf.scene;
-          scene2.add(model2);
-        
-          // Recorrer todos los materiales del modelo y configurarlos como ToonMaterial
+          
+          // Recorremos todos los materiales del modelo
           model2.traverse((child) => {
             if (child.isMesh) {
-              const toonMaterial = new THREE.MeshToonMaterial({
-                color: child.material.color, // Mantener el color original del material
-                specular: new THREE.Color(0x000000), // Especificar un color negro para el componente especular
-              });
-              child.material = toonMaterial; // Reemplazar el material existente por el nuevo material toon
+              // Verificamos si el material tiene texturas definidas
+              if (child.material.map !== null) {
+                // Si hay texturas, las asignamos manualmente al material
+                const texture = child.material.map;
+                child.material = new THREE.MeshBasicMaterial({ map: texture });
+              }
             }
-          });       
-        
-          // Crear un objeto para agrupar la luz y luego rotarlo
-          const lightGroup = new THREE.Object3D();
-        
-          // Agregar una luz direccional al grupo de luces
-          const light = new THREE.DirectionalLight(0xffffff, 1); // Color blanco y intensidad 5
-          light.position.set(2, 1, 2); // Posición de la luz respecto al grupo
-          lightGroup.add(light);
-        
-          // Rotar el grupo de luces en el eje Z
-          lightGroup.rotateZ(Math.PI / 2); // Rotar la luz 45 grados en el eje Z
+          });
           
-          // Agregar el grupo de luces a la escena
-          scene2.add(lightGroup);
-        
-          // Posicionar la cámara y los controles orbitales para el modelo2
-          camera2.position.set(-0.1, 5, 1.8); // Mover la cámara hacia abajo en el eje Z
-          // Mover el objetivo de la cámara
-          controls2.update(); // Actualizar los controles
+          // Agregamos el modelo a la escena
+          scene2.add(model2);
+          
+          // Agregamos una luz de ambiente a la escena
+          const ambientLight = new THREE.AmbientLight(0xffffff, 1); // Color blanco y intensidad 1
+          scene2.add(ambientLight);
+          
+          // Posicionamos la cámara y los controles
+          camera.position.set(-0.1, 5, 1.8);
+          controls.update();
         });
+        
+        
         
 
         loader.load('/3dmodels/prop.glb', function (gltf) {
